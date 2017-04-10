d753b258c23b7c35ce320c98b5297961547a839e
Matthew Ström <matthew.h.strom@gmail.com>
2017-04-10 18:10:48 -0400

    add surface book and apple watch, minify images

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,6 +1,6 @@
 require('../styles/main.css');
 
-if (module.hot) module.hot.accept();
+if(module.hot) module.hot.accept();
 
 const config = {
     apiKey: 'AIzaSyC5OpunsJdVqQfGRssTKWWPPx-HsNp2vcc',
@@ -15,66 +15,98 @@ firebase.initializeApp(config);
 const database = firebase.database();
 
 const devices = [{
-    name: 'iMac',
-    prettyName: 'iMac',
-    image: {
-        fileName: 'Apple%20iMac.png',
-        width: 2788,
-        height: 2351,
-        format: 'png'
+        name: 'iMac',
+        prettyName: 'iMac',
+        image: {
+            fileName: 'Apple%20iMac.png',
+            width: 2788,
+            height: 2351,
+            format: 'png'
+        },
+        screen: {
+            top: 6.5,
+            right: 4,
+            bottom: 32.1,
+            left: 4
+        }
+    }, {
+        name: 'MacbookPro15',
+        prettyName: 'Macbook Pro 15"',
+        image: {
+            fileName: 'Apple%20Macbook%20Pro%2015%22.png',
+            width: 3783,
+            height: 2268,
+            format: 'png'
+        },
+        screen: {
+            top: 7.8,
+            right: 12,
+            bottom: 12.6,
+            left: 12
+        }
+    }, {
+        name: 'Nokia230',
+        prettyName: 'Nokia 230',
+        image: {
+            fileName: 'Nokia%20230%20Black.png',
+            width: 297,
+            height: 705,
+            format: 'png'
+        },
+        screen: {
+            top: 9.4,
+            right: 9.5,
+            bottom: 45.1,
+            left: 10
+        }
+    }, {
+        name: 'iPhone6sGray',
+        prettyName: 'iPhone 6s Space Gray',
+        image: {
+            fileName: 'Apple%20iPhone%206s%20Space%20Gray.png',
+            width: 990,
+            height: 1934,
+            format: 'png'
+        },
+        screen: {
+            top: 15.5,
+            right: 12,
+            bottom: 15.5,
+            left: 12
+        }
+    }, {
+        name: 'SurfaceBook',
+        prettyName: 'Microsoft Surface Book',
+        image: {
+            fileName: 'Microsoft%20Surface%20Book.png',
+            width: 4143,
+            height: 2456,
+            format: 'png'
+        },
+        screen: {
+            top: 7.6,
+            right: 13.7,
+            bottom: 11,
+            left: 13.9
+        }
     },
-    screen: {
-        top: 6.5,
-        right: 4,
-        bottom: 32.1,
-        left: 4
+    {
+        name: 'AppleWatch',
+        prettyName: 'Apple Watch 42mm Edition',
+        image: {
+            fileName: 'Apple%20Watch%2042mm%20Edition%20Closed.png',
+            width: 512,
+            height: 950,
+            format: 'png'
+        },
+        screen: {
+            top: 29.5,
+            right: 19.3,
+            bottom: 29.5,
+            left: 19.4
+        }
     }
-}, {
-    name: 'MacbookPro15',
-    prettyName: 'Macbook Pro 15"',
-    image: {
-        fileName: 'Apple%20Macbook%20Pro%2015%22.png',
-        width: 3783,
-        height: 2268,
-        format: 'png'
-    },
-    screen: {
-        top: 7.8,
-        right: 12,
-        bottom: 12.6,
-        left: 12
-    }
-}, {
-    name: 'Nokia230',
-    prettyName: 'Nokia 230',
-    image: {
-        fileName: 'Nokia%20230%20Black.png',
-        width: 297,
-        height: 705,
-        format: 'png'
-    },
-    screen: {
-        top: 9.4,
-        right: 9.5,
-        bottom: 45.1,
-        left: 10
-    }
-}, {
-    name: 'iPhone6sGray',
-    prettyName: 'iPhone 6s Space Gray',
-    image: {
-        fileName: 'Apple%20iPhone%206s%20Space%20Gray.png',
-        width: 990,
-        height: 1934,
-        format: 'png'
-    },
-    screen: {
-        top: 15.5,
-        right: 12,
-        bottom: 15.5,
-        left: 12
-    }
-}];
+];
 
 const state = {
     device: {},
@@ -84,9 +116,9 @@ const state = {
 const urlParams = new URLSearchParams(window.location.search);
 
 const getDeviceData = (deviceName) => {
-    for (const i in devices) {
+    for(const i in devices) {
         const thisDevice = devices[i];
-        if (thisDevice.name === deviceName)
+        if(thisDevice.name === deviceName)
             return thisDevice;
     }
 };
@@ -96,7 +128,7 @@ const resizeScreen = () => {
     const deviceEl = document.querySelector('.device');
     const deviceWidth = deviceEl.getBoundingClientRect().width;
     const deviceHeight = deviceEl.getBoundingClientRect().height;
-    if (deviceWidth / deviceEl.getBoundingClientRect().height > state.device.image.width / state.device.image.height) {
+    if(deviceWidth / deviceEl.getBoundingClientRect().height > state.device.image.width / state.device.image.height) {
         // height is bound
         const imageWidth = deviceHeight * (state.device.image.width / state.device.image.height);
         const imageHeight = deviceHeight;
@@ -121,7 +153,7 @@ const setScreenImage = (url, update = true) => {
     state.url = url;
     document.querySelector('.device--screen-image').setAttribute('src', url);
     urlParams.set('image', url);
-    if (update)
+    if(update)
         window.history.pushState(state, '', `?${urlParams.toString()}`);
 };
 
@@ -130,7 +162,7 @@ const setDevice = (deviceName, update = true) => {
     const deviceEl = document.querySelector('.device');
     deviceEl.style.backgroundImage = `url(assets/images/${state.device.image.fileName}`;
     urlParams.set('device', deviceName);
-    if (update)
+    if(update)
         window.history.pushState(state, '', `?${urlParams.toString()}`);
     resizeScreen();
 };
@@ -138,17 +170,17 @@ const setDevice = (deviceName, update = true) => {
 const populateSettings = () => {
     const urlInput = document.querySelector('.device--url');
     const deviceInput = document.querySelector('.device--selector');
-    for (const i in devices) {
+    for(const i in devices) {
         const thisDevice = devices[i];
         const thisOpt = document.createElement('option');
         thisOpt.value = thisDevice.name;
         thisOpt.innerText = thisDevice.prettyName;
-        if (thisOpt.value === state.device.name)
+        if(thisOpt.value === state.device.name)
             thisOpt.selected = true;
         deviceInput.appendChild(thisOpt);
     }
 
-    if (urlParams.get('image').length > 0)
+    if(urlParams.get('image').length > 0)
         urlInput.value = urlParams.get('image');
 };
 
@@ -217,7 +249,7 @@ const bindDropdownClick = () => {
     const dropdown = document.querySelector('.dropdown');
     toggle.addEventListener('click', dropdownHandler);
     document.addEventListener('click', () => {
-        if (dropdown.classList.contains('is-active'))
+        if(dropdown.classList.contains('is-active'))
             dropdown.classList.remove('is-active');
     });
     dropdown.addEventListener('click', (e) => {
@@ -228,7 +260,7 @@ const bindDropdownClick = () => {
 document.addEventListener('DOMContentLoaded', function() {
     const deviceName = urlParams.get('device');
     const url = urlParams.get('image');
-    if (urlParams.get('id')) {
+    if(urlParams.get('id')) {
         restoreState(urlParams.get('id'));
     } else {
         setDevice(deviceName || devices[0].name);
